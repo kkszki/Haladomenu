a0689dabdea0bac40ebb8ae193b5271a511a5a86
Imre <nemethi@kkszki.hu>
2025-09-18 09:07:27 +0200

    A menü kidolgozva: bekérés, kiírás, hibás szám megadása esetén, addig kérjük, amíg nem jó a szám, kilépés

--- Haladomenu/Program.cs
+++ Haladomenu/Program.cs
@@ -8,10 +8,145 @@ namespace Haladomenu
 {
     internal class Program
     {
-        
-        static void Main(string[] args)
+
+
+
+        static void Main()
         {
-            
+            string nev = "";
+            int szulEv = 0;
+            bool ffi = true;
+            int currentPoint = 0;
+            double magassag = 1.7803;
+            do
+            {
+                bool selected = false;
+                do
+                {
+
+                    ShowMenu(currentPoint);
+                    switch (Console.ReadKey().Key)
+                    {
+                        case ConsoleKey.Enter:
+                            selected = true;
+                            break;
+
+
+                        case ConsoleKey.UpArrow:
+                            if (currentPoint > 0)
+                            {
+                                currentPoint--;
+                            }
+                            break;
+
+                        case ConsoleKey.DownArrow:
+                            if (currentPoint < 2)
+                            {
+                                currentPoint++;
+                            }
+                            break;
+
+
+
+
+                        default:
+                            Console.Beep();
+                            break;
+                    }
+
+                } while (!selected);
+
+                switch (currentPoint)
+                {
+                    case 0:  //adatbekérés
+                        Console.Clear();
+                        Console.Beep();
+                        Console.WriteLine("Kérem adja meg a nevét: ");
+                        nev = Console.ReadLine();
+                        bool  good = false;
+                        do {
+                            try
+                            {
+                                Console.WriteLine("Kérem adja meg a születési évét: ");
+                                string SzulStr = Console.ReadLine();
+                                szulEv = Convert.ToInt32(SzulStr);
+                                good = true;
+                            }
+                            catch (Exception ex) {
+                                Console.WriteLine("Számmal adja meg");
+                            } 
+                        }while (!good);
+
+                        Console.WriteLine("Kérem adja meg, hogy férfi-e: (i/n)");
+                        ffi = Console.ReadLine().ToLower() == "i";
+                        Console.WriteLine("Az adatokat sikeresen rögzítettük enterre tovább");
+
+                       
+                        Console.ReadLine(); 
+                        break;
+
+                    case 1:  // adatkiírás
+                        Console.Clear();
+                        Console.WriteLine("adatai:");
+                        Console.WriteLine($"A neve: {nev}");
+                        Console.WriteLine($"Születési éve: {szulEv}");
+                        Console.WriteLine($"Neme: {(ffi ? "Férfi" : "Nő")}");
+                        Console.WriteLine($"Magasság: {magassag:F2} m");
+                        Console.WriteLine("Enterre tovább");
+
+                        Console.ReadLine();
+                        break;
+
+                    case 2:  //kilépés
+                        Console.Clear();
+                        Console.WriteLine("Biztosan kilép (i/n): ");
+                        if (Console.ReadKey().Key != ConsoleKey.I)
+                        {
+                            currentPoint = 0;
+                        }
+                        Console.Beep();
+                        break;
+
+                }
+            } while (currentPoint!=2);
+
+        }
+
+        static void ShowMenu(int cPoint)
+        {
+            Console.Clear();
+            if (cPoint == 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            Console.WriteLine("1 - Személyes adatok bekérése");
+
+            if (cPoint == 1)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            Console.WriteLine("2 - Személyes adatok kiírása");
+
+            if (cPoint == 2)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            Console.WriteLine("3 - Kilépés");
+
+            Console.ForegroundColor = ConsoleColor.White;
+
         }
     }
 }
